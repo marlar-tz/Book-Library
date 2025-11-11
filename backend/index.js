ef385d543ba769543bf3b0a3ab2db7fae64cb0b8
marlar-tz <marlarhal55@gmail.com>
2025-11-11 12:29:14 +0700

    Fetched Data

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 
 const bookRoutes = require('./routes/books')
 
@@ -10,6 +11,7 @@ const app = express()
 
 // middleware
 app.use(express.json())
+app.use(cors())
 
 
 app.use((req, res, next) => {
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,32 @@
-const Home = () =>{
-    return(
+import { useEffect, useState } from 'react'
+
+const Home = () => {
+
+    const [books, setBooks] = useState(null);
+
+    useEffect(() => {
+        const fetchBooks = async () => {
+            const response = await fetch('/api/books')
+            const json = await response.json()
+
+            if (response.ok) {
+                setBooks(json)
+            }
+        }
+
+        fetchBooks()
+    }, [])
+
+    return (
         <div className="home">
-            <h2>Home</h2>
+            <div className="books">
+                {books && books.map((book) => {
+                    return <p key={book._id}>{book.title}</p>
+
+                })}
+            </div>
         </div>
-    )
+    );
 }
 
 export default Home
